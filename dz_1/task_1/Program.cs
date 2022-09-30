51b01758e5b0145b82e4f168e30cb37672364625
Valeria <kilimnikvaleria@gmail.com>
2022-09-30 19:51:55 +0300

    homework 1 added

---
+++ dz_1/task_1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+int number1, number2;
+
+Console.WriteLine("Введите 2 целых числа и программа скажет какое из них больше");
+
+Console.WriteLine("Введите первое число: ");
+
+while (!int.TryParse(Console.ReadLine(), out number1))
+{
+    Console.WriteLine("Ошибка ввода! Введите целое первое число");
+}
+
+Console.WriteLine("Введите второе число: ");
+
+while (!int.TryParse(Console.ReadLine(), out number2))
+{
+    Console.WriteLine("Ошибка ввода! Введите целое второе число");
+}
+
+if(number1 > number2)
+{
+    Console.WriteLine("Первое число больше второго");
+}
+else if (number1 < number2)
+{
+    Console.WriteLine("Первое число меньше второго");
+}
+else
+{
+    Console.WriteLine("Оба числа равны");
+}
---
+++ dz_1/task_2/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+int number1, number2, number3;
+
+Console.WriteLine("Введите 3 целых числа и программа выведет максимальное");
+
+Console.WriteLine("Введите первое число: ");
+
+while (!int.TryParse(Console.ReadLine(), out number1))
+{
+    Console.WriteLine("Ошибка ввода! Введите целое первое число");
+}
+
+Console.WriteLine("Введите второе число: ");
+
+while (!int.TryParse(Console.ReadLine(), out number2))
+{
+    Console.WriteLine("Ошибка ввода! Введите целое второе число");
+}
+
+Console.WriteLine("Введите третье число: ");
+
+while (!int.TryParse(Console.ReadLine(), out number3))
+{
+    Console.WriteLine("Ошибка ввода! Введите целое третье число");
+}
+
+int max = number1;
+
+if (number2 > max) {
+    max = number2;
+}
+
+if (number3 > max) {
+    max = number3;
+}
+
+//Или можно сделать с помощью Math
+//int max = Math.Max(number1, Math.Max(number2, number3));
+
+Console.WriteLine($"Максимальное число: {max}");
