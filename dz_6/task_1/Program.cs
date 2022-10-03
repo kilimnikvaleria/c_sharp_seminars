c09130b0635464d12d8bf8f11730fba85809a21d
Valeria <kilimnikvaleria@gmail.com>
2022-10-03 21:53:04 +0300

    homework 6

---
+++ dz_6/task_1/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int GetArraySize () 
+{
+    Console.Write($"Введите размер массива (целое положительное число): ");
+    int size;
+
+    while (!int.TryParse(Console.ReadLine(), out size) || (size < 0))
+    {
+        Console.WriteLine($"Ошибка ввода! Введите целое положительное число:");
+    }
+
+    return size;
+}
+
+int[] GetArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите целое число (элемент массива номер {i}): ");
+
+        while (!int.TryParse(Console.ReadLine(), out array[i]))
+        {
+            Console.WriteLine($"Ошибка ввода! Введите целое число (элемент массива номер {i}):");
+        }
+    }
+
+    return array;
+}
+
+int GetQuantityOfPositiveNumbers(int[] array)
+{
+    var quantity = 0;
+
+    for (int i = 0; i < array.Length; i++ ) 
+    {
+        if (array[i] > 0) 
+        {
+            quantity++;
+        }
+    }
+
+    return quantity;
+}
+
+var size = GetArraySize();
+var array = GetArray(size);
+var quantity = GetQuantityOfPositiveNumbers(array);
+Console.Write($"Колличество положительных чисел, введенных пользователем равно: {quantity}");
+
---
+++ dz_6/task_2/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+double GetСoefficient(string coefficientName)
+{
+    Console.Write($"Введите коэффициент {coefficientName}: ");
+    
+    double coefficient;
+
+    while (!double.TryParse(Console.ReadLine(), out coefficient))
+    {
+        Console.WriteLine($"Ошибка ввода! Введите коэффициент {coefficientName}:");
+    }
+
+    return coefficient;
+}
+
+string GetPointOfIntersection(double b1, double k1, double b2, double k2) 
+{
+    if (b1==b2 & k1==k2) 
+    {
+        return"Прямые совпадают";
+    }
+    
+    if (k1==k2) 
+    {
+        return"Прямые параллельны друг другу";
+    } 
+
+    var xIntersection = (b2-b1)/(k1-k2);
+    var yIntersection = k1*xIntersection + b1;
+
+    return "Точка пересечения"+" ("+ Math.Round(xIntersection, 2)+" ;"+Math.Round(yIntersection, 2)+ ")";
+}
+
+double b1 = GetСoefficient("b1");
+double k1 = GetСoefficient("k1");
+double b2 = GetСoefficient("b2");
+double k2 = GetСoefficient("k2");
+
+Console.Write(GetPointOfIntersection(b1, k1, b2, k2));
